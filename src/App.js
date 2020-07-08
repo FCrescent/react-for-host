2f02eddff2022ec0ea8da124382402421410bc71
FCrescent <fabiencrescent04@gmail.com>
2020-07-08 05:00:58 +0200

    commit pour "Hello World !" et "homepage" dans package.json

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+      <div className="App">
+
+
+          <h1>Hello World! zip</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
