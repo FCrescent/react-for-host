1824838e3161d95e5580535a74bc1a2b9dd8e6e4
FCrescent <fabiencrescent04@gmail.com>
2020-07-10 19:47:32 +0200

    En cours : mise en place "Navigation"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import fastSymbol from './fast.svg';
+import gearSymbol from './gear-symbol.svg';
 //import eaglePic from './eagle-photo.jpg';
 import './App.css';
 import './App.scss';
@@ -8,7 +10,19 @@ import './App.scss';
 function App() {
   return (
       <div className="App">
-
+          <div className="Nav-menu">
+          <a href="#">
+            <span class="square">
+              <img src={fastSymbol} alt="man running" />
+            </span>
+          </a>
+          <ul>
+              <li>Up !</li>
+              <li>About</li>
+              <li>Des liens </li>
+              <li></li>
+            </ul>
+          </div>
       {/*<h1>Hello World!!!!!! zip55</h1>*/}
       <header className="App-header">
       <div className="Hero-header">
@@ -21,6 +35,7 @@ function App() {
         {/* <img src={logo} className="App-logo" alt="logo" /> */  }
       <div className="My-bio">
         <h1>En quelques mots...</h1>
+        {/*METTRE UN rotating gear pas loin*/}
       </div>
         <p>------------------------------</p>
         <p>Pour le passage de Atom/Local à Hostinger/Online, suivre ce lien :</p>
@@ -32,6 +47,16 @@ function App() {
         >
         Deploying a React Application on Hostinger
         </a>
+        <h3>Le protocole en détails : </h3>
+        <ol>
+          <li>Git commit & push depuis "react-for-host"</li>
+          <li>A partir de cmd/"react-for-host" >>> npm run build</li>
+          <li>copy-paste les éléments du build dans "react-for-host-build"</li>
+          <li>Ouvrir Atom/Ouvrir folder "build"</li>
+          <li>Git commit & push depuis "react-for-host-build"</li>
+          <li>Go Hostinger -puis- "Git" -puis- Click "Déployer"</li>
+        </ol>
+
         <p>------------------------------</p>
 
           <p>Pour React Multi pages, suivre ce lien :</p>
@@ -45,7 +70,7 @@ function App() {
           </a>
           <p>------------------------------</p>
 
-          <p> To start, go on cmd and do "d:" then "cd react-for-host" and finally "npm start"</p>
+          <p> To start the local coding, go on cmd and do "d:" then "cd react-for-host" and finally "npm start"</p>
 
 
           <p>------------------------------</p>
@@ -57,6 +82,9 @@ function App() {
         >
           Learn React
         </a>
+
+        <p>------------------------------</p>
+        <p> Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a></p>
       </header>
     </div>
   );
