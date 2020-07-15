0519aabb88a083d7aea471e2b8aee99727e7270f
FCrescent <fabiencrescent04@gmail.com>
2020-07-15 09:08:16 +0200

    Déplacement contenu page vers ReadMe Github

--- src/App.js
+++ src/App.js
@@ -72,52 +72,6 @@ function App() {
         {/*METTRE UN rotating gear pas loin*/}
       </div>
         <p>------------------------------</p>
-        <p>Pour le passage de Atom/Local à Hostinger/Online, suivre ce lien :</p>
-        <a
-        className="App-link"
-        href="https://medium.com/@kmchaplain/deploying-a-react-application-on-hostinger-cf2c6c43b072"
-        target="_blank"
-        rel="noopener noreferrer"
-        >
-        Deploying a React Application on Hostinger
-        </a>
-        <h3>Le protocole en détails : </h3>
-        <ol>
-          <li>Git commit & push depuis "react-for-host"</li>
-          <li>A partir de cmd/"react-for-host" >>> npm run build</li>
-          <li>copy-paste les éléments du build dans "react-for-host-build"</li>
-          <li>Ouvrir Atom/Ouvrir folder "build"</li>
-          <li>Git commit & push depuis "react-for-host-build"</li>
-          <li>Go Hostinger -puis- "Git" -puis- Click "Déployer" (si bug, supprimer le dépot Git sur Hostinger puis recréer & déployer)</li>
-        </ol>
-
-        <p>------------------------------</p>
-
-          <p>Pour React Multi pages, suivre ce lien :</p>
-          <a
-          className="App-link"
-          href="https://reactrouter.com/web/guides/quick-start"
-          target="_blank"
-          rel="noopener noreferrer"
-          >
-          React Router: Declarative Routing for React.js - React Training
-          </a>
-          <p>------------------------------</p>
-
-          <p> To start the local coding, go on cmd and do "d:" then "cd react-for-host" and finally "npm start"</p>
-
-
-          <p>------------------------------</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-
-        <p>------------------------------</p>
         <p> Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a></p>
         <p><span>Photo by <a href="https://unsplash.com/@itfeelslikefilm?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">🇸🇮 Janko Ferlič</a> on <a href="https://unsplash.com/s/photos/library?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span></p>
 
